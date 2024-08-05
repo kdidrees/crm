eed300d5b78d2030203dc47c0493df39959649bc
khurshid <Khurshid@qurilo.com>
2024-08-05 13:32:59 +0530

    update

---
+++ src/components/LeadForm.jsx
@@ -0,0 +1,162 @@
+import React from "react";
+
+export default function LeadForm() {
+  return (
+    <>
+      <section className="max-w-4xl p-6 mx-auto bg-indigo-600 rounded-md shadow-md dark:bg-gray-800 mt-20">
+        <h1 className="text-xl font-bold text-white capitalize dark:text-white">
+          Account settings
+        </h1>
+        <form>
+          <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+            <div>
+              <label
+                className="text-white dark:text-gray-200"
+                htmlFor="username"
+              >
+                Username
+              </label>
+              <input
+                id="username"
+                type="text"
+                placeholder="please enter profile name"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+              />
+            </div>
+            <div>
+              <label
+                className="text-white dark:text-gray-200"
+                htmlFor="emailAddress"
+              >
+                Email Address
+              </label>
+              <input
+                id="emailAddress"
+                type="email"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+              />
+            </div>
+
+            <div>
+              <label
+                className="text-white dark:text-gray-200"
+                htmlFor="passwordConfirmation"
+              >
+                Mobile
+              </label>
+              <input
+                id="color"
+                type="tel"
+                value={`+91`}
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+              />
+            </div>
+            <div>
+              <label
+                className="text-white dark:text-gray-200"
+                htmlFor="passwordConfirmation"
+              >
+                Source
+              </label>
+              <select className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring">
+                <option>website</option>
+                <option>offline</option>
+                <option>trade show</option>
+                <option>indiamart</option>
+              </select>
+            </div>
+         
+      
+            <div>
+              <label
+                className="text-white dark:text-gray-200"
+                htmlFor="passwordConfirmation"
+              >
+                Remarks
+              </label>
+              <textarea
+                id="textarea"
+                type="textarea"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+                defaultValue={""}
+              />
+            </div>
+         
+          </div>
+          <div className="flex justify-end mt-6">
+            <button className="px-6 py-2 leading-5 text-white transition-colors duration-200 transform bg-pink-500 rounded-md hover:bg-pink-700 focus:outline-none focus:bg-gray-600">
+              Save
+            </button>
+          </div>
+        </form>
+      </section>
+      <section className="max-w-4xl p-6 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800 mt-20">
+        <h2 className="text-lg font-semibold text-gray-700 capitalize dark:text-white">
+          Account settings
+        </h2>
+        <form>
+          <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+            <div>
+              <label
+                className="text-gray-700 dark:text-gray-200"
+                htmlFor="username"
+              >
+                Username
+              </label>
+              <input
+                id="username"
+                type="text"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+              />
+            </div>
+            <div>
+              <label
+                className="text-gray-700 dark:text-gray-200"
+                htmlFor="emailAddress"
+              >
+                Email Address
+              </label>
+              <input
+                id="emailAddress"
+                placeholder="enter email address"
+                type="email"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+              />
+            </div>
+            <div>
+              <label
+                className="text-gray-700 dark:text-gray-200"
+                htmlFor="password"
+              >
+                Password
+              </label>
+              <input
+                id="password"
+                type="password"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+              />
+            </div>
+            <div>
+              <label
+                className="text-gray-700 dark:text-gray-200"
+                htmlFor="passwordConfirmation"
+              >
+                Password Confirmation
+              </label>
+              <input
+                id="passwordConfirmation"
+                type="password"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+              />
+            </div>
+          </div>
+          <div className="flex justify-end mt-6">
+            <button className="px-6 py-2 leading-5 text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
+              Save
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
+}
